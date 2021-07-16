87628c430c0895c0e05b2fc95594769f51dfb88d
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2021-07-16 10:10:33 +0200

    Clean up home view css

--- src/components/Home/Index.vue
+++ src/components/Home/Index.vue
@@ -5,25 +5,29 @@
       <h2 class="is-size-2">Talk <strong>Really</strong> Pseudonymously</h2>
     </div>
 
-    <div class="has-text-centered mt-4 mb-4">
+    <div class="has-text-centered my-4">
       <router-link class="button button-ok" :to="{ name: 'talk.index' }">
         <h2 class="is-size-2 has-text-weight-bold">Start</h2>
       </router-link>
     </div>
 
-    <div class="home-view-who-we-are-banner has-text-centered">
-      <h4 class="is-size-4">Who We Are</h4>
+    <div class="home-view-who-we-are-banner home-view-background-base has-text-centered py-4">
+      <div class="container px-2">
+        <div class="is-flex is-flex-direction-column is-align-items-center p-4">
+          <h4 class="is-size-4">Who We Are</h4>
 
-      <h5 class="is-size-5">
-        The Berkman Klein Center's mission is to explore and understand cyberspace; to study its
-        development, dynamics, norms, and standards; and to assess the need or lack thereof for laws
-        and sanctions.
-      </h5>
-      <h5 class="is-size-5">
-        We are a research center, premised on the observation that what we seek to learn is not
-        already recorded. Our method is to build out into cyberspace, record data as we go,
-        self-study, and share. Our mode is entrepreneurial nonprofit.
-      </h5>
+          <h5 class="is-size-5">
+            The Berkman Klein Center's mission is to explore and understand cyberspace; to study its
+            development, dynamics, norms, and standards; and to assess the need or lack thereof for
+            laws and sanctions.
+          </h5>
+          <h5 class="is-size-5">
+            We are a research center, premised on the observation that what we seek to learn is not
+            already recorded. Our method is to build out into cyberspace, record data as we go,
+            self-study, and share. Our mode is entrepreneurial nonprofit.
+          </h5>
+        </div>
+      </div>
     </div>
 
     <div class="columns">
@@ -45,32 +49,34 @@
       </div>
     </div>
 
-    <div class="home-view-trending has-text-centered">
-      <div class="columns is-4">
-        <div class="column">
-          <div>
-            <h4 class="is-size-4">Trending Topics</h4>
+    <div class="home-view-trending home-view-background-base has-text-centered py-4">
+      <div class="container px-2">
+        <div class="columns is-4 p-4">
+          <div class="column">
+            <div>
+              <h4 class="is-size-4">Trending Topics</h4>
 
-            <ul>
-              <li>Something cool</li>
-              <li>Something cool</li>
-              <li>Something cool</li>
-              <li>Something cool</li>
-              <li>Something cool</li>
-            </ul>
+              <ul>
+                <li>Something cool</li>
+                <li>Something cool</li>
+                <li>Something cool</li>
+                <li>Something cool</li>
+                <li>Something cool</li>
+              </ul>
+            </div>
           </div>
-        </div>
-        <div class="column">
-          <div>
-            <h4 class="is-size-4">Trending Threads</h4>
+          <div class="column">
+            <div>
+              <h4 class="is-size-4">Trending Threads</h4>
 
-            <ul>
-              <li>Something cool</li>
-              <li>Something cool</li>
-              <li>Something cool</li>
-              <li>Something cool</li>
-              <li>Something cool</li>
-            </ul>
+              <ul>
+                <li>Something cool</li>
+                <li>Something cool</li>
+                <li>Something cool</li>
+                <li>Something cool</li>
+                <li>Something cool</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
