9544a3d97805f48e1b0e6dc6cb0ccb537b244f2a
Bimal Gharti Magar <bimal@endpointdev.com>
2021-12-16 17:05:48 +0545

    Added search and sort for featured channel
    * Emitting search text from search box
    * Using search text to filter channel
    * Using filtered channels to sort

--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -1,7 +1,17 @@
 <template>
   <input
+    v-model="searchText"
     class="rounded border-2 border-gray-500 w-full h-12 px-3 text-xl"
     type="text"
     placeholder="Search for a channel by keyword"
+    @keyup="emit('update-search', searchText)"
   />
 </template>
+
+<script setup>
+import { ref } from "vue";
+
+const searchText = ref("");
+
+const emit = defineEmits(["update-search"]);
+</script>
--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,20 +1,33 @@
 <template>
   <div class="mx-auto container-width">
-    <SearchBox class="my-5" />
-    <Suspense>
-      <template #default> <FeaturedChannelList /> </template>
-      <template #fallback>Loading channels...</template>
-    </Suspense>
+    <SearchBox @update-search="updateSearch" class="my-5" />
+    <FeaturedChannelList :channels="channels" />
     <div class="flex justify-end my-4">
       <CreateChannel />
     </div>
   </div>
 </template>
-<script>
+<script setup>
 import SearchBox from "../components/SearchBox.vue";
 import FeaturedChannelList from "../components/Channels/FeaturedChannelList.vue";
 import CreateChannel from "../components/Channels/CreateChannel.vue";
-export default {
-  components: { SearchBox, FeaturedChannelList, CreateChannel },
-};
+import { computed, onMounted, ref } from "vue";
+import useStore from "../composables/global/useStore";
+const { loadChannels, getChannels } = useStore;
+
+const searchText = ref("");
+const channels = computed(() => {
+  const filteredChannels = getChannels.value.filter((x) => {
+    if (searchText.value.trim().length == 0) {
+      return true;
+    } else {
+      return x.name?.toLowerCase().indexOf(searchText.value) > -1;
+    }
+  });
+  return filteredChannels;
+});
+
+onMounted(async () => await loadChannels());
+
+const updateSearch = (value) => (searchText.value = value?.toLowerCase());
 </script>
