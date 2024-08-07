d8197b864e5e39ea13378f0d179993ce05c2b82a
Boaz Sender <boazeliezer@gmail.com>
2024-08-07 11:49:26 -0700

    Remove dead time consts from utils. Supports #45

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,3 @@
-const WEEK_IN_MILLIS = 6.048e8,
-  DAY_IN_MILLIS = 8.64e7,
-  HOUR_IN_MILLIS = 3.6e6,
-  MIN_IN_MILLIS = 6e4,
-  SEC_IN_MILLIS = 1e3;
-
 export default {
   changeToTitle: function (str) {
     return str.length > 0 ? str.charAt(0).toUpperCase() + str.slice(1) : str;
