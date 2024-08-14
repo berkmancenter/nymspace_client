c9e016c8e8c7d05ba69d2351ee181270f743edeb
Boaz Sender <boazeliezer@gmail.com>
2024-08-14 16:50:02 -0700

    Attempt some mobile polish. Supports #45.

--- src/components/Shared/HeaderSection.vue
+++ src/components/Shared/HeaderSection.vue
@@ -1,9 +1,9 @@
 <template>
   <div
-    class="px-3 pb-1 text-gray-500 font-bold hover:text-gray-900 flex justify-between"
+    class="px-3 py-1 text-gray-500 font-bold hover:text-gray-900 flex justify-between"
   >
     <div>
-      <router-link to="/" class="text-4xl">
+      <router-link to="/" class="text-base sm:text-4xl">
         <p class="inline-block text-red-600">nymspace</p>
       </router-link>
       <a
@@ -21,17 +21,18 @@
         to="/login"
         class="hover:text-red-600 text-black"
         title="Retain a pseudonym across sessions"
-      >Login</router-link>
+        >Login</router-link
+      >
 
       <router-link to="/" class="">
-        <p class="inline-block hover:text-red-600 text-black">All Channels</p>
+        <p class="inline-block hover:text-red-600 text-black">Channels</p>
       </router-link>
       <a
         class="hover:text-red-600 text-black"
         href="https://cyber.harvard.edu/projects/nymity"
         rel="noopener"
         target="_blank"
-        >About Nymity<ExternalLinkIcon class="ml-1 inline-block w-4 h-4" />
+        >About <ExternalLinkIcon class="ml-1 inline-block w-4 h-4" />
       </a>
     </div>
   </div>
@@ -40,9 +41,7 @@
 import store from "../../composables/global/useStore";
 import { ExternalLinkIcon } from "@heroicons/vue/outline";
 import useStore from "../../composables/global/useStore";
-const {
-  getGuestStatus
-} = useStore;
+const { getGuestStatus } = useStore;
 const { getMajorError } = store;
 const showVersion = import.meta.env.VITE_SHOW_VERSION === "true";
 const version = __APP_VERSION__;
