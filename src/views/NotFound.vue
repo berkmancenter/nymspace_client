bb05a6f6e2e51b3b9f7ef864513bc1b94e5a08f8
Boaz Sender <boazeliezer@gmail.com>
2024-08-22 13:21:28 -0700

    Carry updated styles over to additional pages. Supports #45.

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,17 +1,19 @@
 <template>
-  <div
-    class="w-full text-7xl text-harvard-red flex justify-center items-center text-center font-bold"
-  >
-    404<br /><br />Oops! Stumbled upon unknown!
-  </div>
-  <div class="text-center my-16 text-2xl">
-    Either the link does not exist or something went wrong.
-  </div>
-  <div class="text-center my-16 text-2xl">
-    Go to homepage
-    <router-link :to="path" class="text-harvard-red underline"
-      >Threads</router-link
+  <div class="flex-1 flex flex-col justify-center">
+    <div
+      class="w-full text-2xl text-harvard-red flex justify-center items-center text-center font-bold"
     >
+      404<br />Oops! Stumbled upon unknown!
+    </div>
+    <div class="text-center text-xl">
+      Either the link does not exist or something went wrong.
+    </div>
+    <div class="text-center text-xl">
+      Go to homepage
+      <router-link :to="path" class="text-harvard-red underline"
+        >Threads</router-link
+      >
+    </div>
   </div>
 </template>
 
