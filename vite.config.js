3da727426d8c87dce2b31235e05f686f04005b78
Boaz Sender <boazeliezer@gmail.com>
2024-08-27 17:22:45 -0700

    Fix env management in vite config. Supports #45.

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,32 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          // Grouping components of home page on single file
-          "home-page": [
-            "./src/layout/HomePageLayout.vue",
-            "./src/layout/LandingPageLayout.vue",
-            "./src/components/Shared/Modal.vue",
-            "./src/views/LandingPage.vue",
-            "./src/components/Banner/LoggedInBanner.vue",
-          ],
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [vue()],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            // Grouping components of home page on single file
+            "home-page": [
+              "./src/layout/HomePageLayout.vue",
+              "./src/layout/LandingPageLayout.vue",
+              "./src/components/Shared/Modal.vue",
+              "./src/views/LandingPage.vue",
+              "./src/components/Banner/LoggedInBanner.vue",
+            ],
+          },
         },
       },
     },
-  },
-  define: {
-    __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
-  },
-  base: process.env.VITE_PATH ? process.env.VITE_PATH : "/",
+    define: {
+      __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
+    },
+    base: env.VITE_PATH ? env.VITE_PATH : "/",
+  };
 });
