5914c238429f7be9951bc264e734cc2d9b2a9f56
Brendan Miller <code+os@brendanmiller.com>
2024-10-09 16:13:21 -0700

    Rename proposal to poll

--- src/views/PollPage.vue
+++ src/views/PollPage.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="flex flex-col justify-center flex-1">
     <div class="flex items-center justify-center w-full text-2xl font-bold text-center text-harvard-red">
-      TODO: Nebula proposal experience here!
+      TODO: Nebula poll experience here!
     </div>
   </div>
 </template>
 
-<script setup>
-const path = import.meta.env.VITE_PATH ? import.meta.env.VITE_PATH : ''
-</script>
+<script setup></script>
