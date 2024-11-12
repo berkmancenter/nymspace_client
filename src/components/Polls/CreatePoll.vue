235575a99f3e43d0593f3bcdadb08347730b915e
katherineqian <21981137+katherineqian@users.noreply.github.com>
2024-11-12 12:33:13 -0800

    Add separate date and time pickers to modal, with functionality to combine date and time after picking them separately. Also adds a note about time zones, though actual time zone support is a TODO.

--- src/components/Polls/CreatePoll.vue
+++ src/components/Polls/CreatePoll.vue
@@ -1,5 +1,5 @@
 <template>
-  <p class="text-gray-600 mb-4">
+  <p class="text-gray-600 text-sm mb-8">
     Submit responses and vote on them anonymously. When a response gets enough votes, voters for that response reveal their
     names.
   </p>
@@ -16,12 +16,26 @@
       </textarea>
     </div>
   </div>
-  <div class="mb-4">
-    <p class="font-semibold">End date:</p>
+  <div class="flex">
+    <div class="w-1/2">
+      <p class="font-semibold">End date:</p>
 
-    <!-- Date picker -->
-    <div class="flex max-w-sm border rounded border-gray-500 w-full">
-      <input v-model="expirationDate" class="rounded w-full p-3" type="date" :min="today" />
+      <!-- Date picker -->
+      <div class="flex max-w-sm border rounded border-gray-500 w-full">
+        <VueDatePicker v-model="selectedDate" :enable-time-picker="false" />
+      </div>
+    </div>
+    <div class="w-4"></div>
+    <div class="w-1/2">
+      <p class="font-semibold">End time:</p>
+
+      <!-- Time picker -->
+      <div class="flex max-w-sm border rounded border-gray-500">
+        <VueDatePicker v-model="selectedTime" time-picker :text-input="timeInputOptions" :is24="false" />
+      </div>
+      <p class="text-gray-600 mt-2 text-sm">
+        Time is in your local timezone. The poll will end at this time for everyone regardless of their time zone.
+      </p>
     </div>
   </div>
   <!-- Error message -->
@@ -32,6 +46,8 @@
 import { ref } from '@vue/reactivity'
 import useStore from '../../composables/global/useStore'
 import { useRoute } from 'vue-router'
+import VueDatePicker from '@vuepic/vue-datepicker'
+import '@vuepic/vue-datepicker/dist/main.css'
 
 const { createPoll } = useStore
 const title = ref('')
@@ -39,12 +55,15 @@ const description = ref('')
 const message = ref('')
 const route = useRoute()
 
-const today = new Date().toISOString().split('T')[0]
-const date = new Date()
-date.setDate(date.getDate() + 5)
-const defaultDate = date.toISOString().split('T')[0]
+const timeInputOptions = {
+  openMenu: false,
+  selectOnFocus: true
+}
 
-const expirationDate = ref(defaultDate)
+// Default date is 5 days in the future.
+const defaultDate = new Date(Date.now() + 5 * 24 * 60 * 60 * 1000)
+const selectedDate = ref(defaultDate)
+const selectedTime = ref({ hours: 23, minutes: 59 })
 
 defineExpose({
   processCreate
@@ -57,11 +76,15 @@ function processCreate() {
     message.value = 'A title is required.'
     return
   }
-  const expirationDateValue = new Date(expirationDate.value)
+
+  const expirationDate = new Date(selectedDate.value)
+  expirationDate.setHours(selectedTime.value.hours)
+  expirationDate.setMinutes(selectedTime.value.minutes)
+
   createPoll({
     title: title.value,
     description: description.value,
-    expirationDate: expirationDateValue,
+    expirationDate,
     topicId: route.params.channelId
   })
     .then((x) => emit('createSuccess'))
@@ -72,3 +95,11 @@ function hasTitle() {
   return title.value.trim().length > 0
 }
 </script>
+
+<style>
+/* Styles used for date picker */
+.dp__theme_light {
+  --dp-primary-color: /*any color you want */ #a41d30;
+  --dp-primary-text-color: /*if the text contrast is not good, you can adjust this variable also*/ #ffffff;
+}
+</style>
