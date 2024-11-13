fb3c7c4fdce4f3614acd3023eb3684ff87a9bea7
katherineqian <21981137+katherineqian@users.noreply.github.com>
2024-11-13 11:04:44 -0800

    Wrap ChannelListItem in a div so it's not a non-element parent.

--- src/components/Channels/ChannelListItem.vue
+++ src/components/Channels/ChannelListItem.vue
@@ -1,37 +1,39 @@
 <template>
-  <router-link v-slot="{ navigate }" custom :to="channelLink">
-    <li
-      :class="getChannelClass(item)"
-      class="flex items-center justify-between gap-x-6 gap-y-4 py-5 sm:flex-nowrap px-2"
-      @click="navigate"
-    >
-      <div class="flex gap-1 items-start">
-        <BookmarkIcon
-          :class="item.isFollowed ? 'fill-current' : ''"
-          class="h-4 4 inline-block cursor-pointer hover:text-black hover:stroke-current"
-          @click.prevent="pinChannel"
-        />
-        <div>
-          <p class="text-sm font-semibold leading-3 text-gray-900 flex gap-2 items-center">
-            {{ item.name }}
-            <LockClosedIcon v-if="isPrivate(item)" class="h-4 w-4" />
-          </p>
-
-          <div class="flex items-center gap-x-2 text-xs leading-5 text-gray-500">
-            <p>
-              Last updated
-              <time :datetime="item.latestMessageCreatedAt">{{
-                new Date(item.latestMessageCreatedAt).toLocaleDateString('en-US', {
-                  hour: '2-digit',
-                  minute: '2-digit'
-                })
-              }}</time>
+  <div>
+    <router-link v-slot="{ navigate }" custom :to="channelLink">
+      <li
+        :class="getChannelClass(item)"
+        class="flex items-center justify-between gap-x-6 gap-y-4 py-5 sm:flex-nowrap px-2"
+        @click="navigate"
+      >
+        <div class="flex gap-1 items-start">
+          <BookmarkIcon
+            :class="item.isFollowed ? 'fill-current' : ''"
+            class="h-4 4 inline-block cursor-pointer hover:text-black hover:stroke-current"
+            @click.prevent="pinChannel"
+          />
+          <div>
+            <p class="text-sm font-semibold leading-3 text-gray-900 flex gap-2 items-center">
+              {{ item.name }}
+              <LockClosedIcon v-if="isPrivate(item)" class="h-4 w-4" />
             </p>
+
+            <div class="flex items-center gap-x-2 text-xs leading-5 text-gray-500">
+              <p>
+                Last updated
+                <time :datetime="item.latestMessageCreatedAt">{{
+                  new Date(item.latestMessageCreatedAt).toLocaleDateString('en-US', {
+                    hour: '2-digit',
+                    minute: '2-digit'
+                  })
+                }}</time>
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-    </li>
-  </router-link>
+      </li>
+    </router-link>
+  </div>
 </template>
 
 <script setup>
