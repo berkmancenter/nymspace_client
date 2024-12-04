c0b7a91e1e448ad3f6083a81a0e1faa0abdb47cc
katherineqian <21981137+katherineqian@users.noreply.github.com>
2024-12-04 14:05:03 -0600

    Add grid with placeholder values for choice inputs.

--- src/views/PollPage.vue
+++ src/views/PollPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col justify-between flex-1 p-4">
-    <div class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <div class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 auto-rows">
       <template v-for="item in items" :key="item.id">
         <ChoiceItem :item="item" :threshold-value="thresholdValue" />
       </template>
@@ -17,21 +17,16 @@ import ChoiceItem from '../components/Polls/ChoiceItem.vue'
 const thresholdValue = 5
 
 const items = ref([
-  { id: 1, text: 'Choice 1' },
-  { id: 2, text: 'Choice 2' },
-  { id: 3, text: 'Choice 3' },
-  { id: 4, text: 'Choice 4' },
-  { id: 5, text: 'Choice 5' },
-  { id: 6, text: 'Choice 6' },
-  { id: 7, text: 'Choice 7' },
-  { id: 8, text: 'Choice 8' },
-  { id: 9, text: 'Choice 9' },
-  { id: 10, text: 'Choice 10' }
+  { id: 1, text: 'See Wicked on Broadway', votes: 3 },
+  { id: 2, text: 'Visit the Natural History Museum', votes: 7 },
+  { id: 3, text: 'Chill at home', votes: 3 },
+  { id: 4, text: 'Go to the Met', votes: 2 },
+  { id: 5, text: 'Go shopping', votes: 5 }
 ])
 </script>
 
 <style scoped>
 .auto-rows {
-  grid-auto-rows: minmax(100px, auto);
+  grid-auto-rows: minmax(40px, auto);
 }
 </style>
