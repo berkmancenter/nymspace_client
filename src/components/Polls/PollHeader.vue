e58898752eb743209710a439543193a71d49d4f8
katherineqian <21981137+katherineqian@users.noreply.github.com>
2024-12-05 10:10:22 -0600

    Show and hide long text.

--- src/components/Polls/PollHeader.vue
+++ src/components/Polls/PollHeader.vue
@@ -12,11 +12,12 @@
           <h2 class="text-lg text-left font-thin truncate">
             <button class="w-full text-left truncate" @click="openThreadModal">What should we do this afternoon?</button>
           </h2>
-          <p class="text-xs text-left">
-            It's our last afternoon in New York! How should we spend it? I really love rambling, so I'll keep this
-            description long. Really, really long. I mean, it's just a poll, but I want to make sure it's clear what I'm
-            asking.
+          <p class="text-sm text-left" :class="{ 'line-clamp-2': !showFullDescription }">
+            {{ description }}
           </p>
+          <button v-if="isLongDescription" class="text-blue-500 text-sm" @click="showFullDescription = !showFullDescription">
+            {{ showFullDescription ? 'hide' : 'show more' }}
+          </button>
           <div class="flex gap-2 mt-2">
             <div class="bg-red-100 text-gray-700 rounded-full px-3 py-1 text-xs font-semibold">
               Threshold: 5
@@ -43,6 +44,7 @@
 </template>
 
 <script setup>
+import { ref, computed } from 'vue'
 import DeleteThread from '../Threads/DeleteThread.vue'
 import EditThread from '../Threads/EditThread.vue'
 import { ViewListIcon } from '@heroicons/vue/outline/'
@@ -61,4 +63,21 @@ defineProps({
     required: true
   }
 })
+
+const showFullDescription = ref(false)
+const description = `It's our last afternoon in New York! How should we spend it? I really love rambling, so I'll keep this
+description long. Really, really long. I mean, it's just a poll, but I want to make sure it's clear what I'm
+asking.`
+
+const isLongDescription = computed(() => description.length > 125)
 </script>
+
+<style scoped>
+.line-clamp-2 {
+  display: -webkit-box;
+  line-clamp: 2;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+}
+</style>
