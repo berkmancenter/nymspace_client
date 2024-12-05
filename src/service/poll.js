eda94adec20e2b10484a791fff5d8b12de69bae4
katherineqian <21981137+katherineqian@users.noreply.github.com>
2024-12-05 10:49:59 -0600

    Implement all remaining endpoints in poll service.

--- src/service/poll.js
+++ src/service/poll.js
@@ -14,12 +14,47 @@ export default {
       })
       .then((x) => x.data)
   },
-
+  // Corresponds to listPolls
   getPolls: async function (channelId) {
     return await axios
       .get(`/polls`, {
         params: {
           topic: channelId
+        }
+      })
+      .then((x) => x.data)
+  },
+  // Corresponds to inspectPoll
+  getPoll: async function (pollId) {
+    return await axios
+      .get(`/polls/${pollId}`, {
+        params: {
+          topic: pollId
+        }
+      })
+      .then((x) => x.data)
+  },
+  getPollResponses: async function (pollId) {
+    return await axios
+      .get(`/polls/${pollId}/responses`, {
+        params: {
+          topic: pollId
+        }
+      })
+      .then((x) => x.data)
+  },
+  respondPoll: async function (payload) {
+    return await axios
+      .post(`/polls/${payload.pollId}/respond`, {
+        ...payload
+      })
+      .then((x) => x.data)
+  },
+  getPollResponseCounts: async function (pollId) {
+    return await axios
+      .get(`/polls/${pollId}/responseCounts`, {
+        params: {
+          topic: pollId
         }
       })
       .then((x) => x.data)
