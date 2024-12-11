05f3f876e72d8351f4da9d447a63cf86cacec119
katherineqian <21981137+katherineqian@users.noreply.github.com>
2024-12-10 22:56:13 -0600

    Don't show "create" button on Create Poll modal if not logged in.

--- src/components/Shared/CreateSpace.vue
+++ src/components/Shared/CreateSpace.vue
@@ -55,6 +55,7 @@
         Cancel
       </button>
       <button
+        v-if="tab === 1 || (tab === 2 && isLoggedIn)"
         class="rounded bg-gray-600 px-2 py-2 font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
         @click="onSubmit"
       >
