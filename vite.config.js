333fbcf903e1300a1b673a1ed22874b874c7e60d
Brendan Miller <code+os@brendanmiller.com>
2024-12-11 13:01:31 -0800

    Change ref from Modal to ThemedModal in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig, loadEnv } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), "");
+  const env = loadEnv(mode, process.cwd(), '')
   return {
     plugins: [vue()],
     build: {
@@ -13,20 +13,21 @@ export default defineConfig(({ command, mode }) => {
         output: {
           manualChunks: {
             // Grouping components of home page on single file
-            "home-page": [
-              "./src/layout/HomePageLayout.vue",
-              "./src/layout/LandingPageLayout.vue",
-              "./src/components/Shared/Modal.vue",
-              "./src/views/LandingPage.vue",
-              "./src/components/Banner/LoggedInBanner.vue",
-            ],
-          },
-        },
-      },
+            'home-page': [
+              './src/layout/HomePageLayout.vue',
+              './src/layout/LandingPageLayout.vue',
+              './src/components/Shared/ThemedModal.vue',
+              './src/views/LandingPage.vue',
+              './src/components/Banner/LoggedInBanner.vue'
+            ]
+          }
+        }
+      }
     },
     define: {
-      __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
+      __APP_VERSION__: JSON.stringify(process.env.npm_package_version)
     },
-    base: `${env.VITE_PATH}/` ? env.VITE_PATH : "/",
-  };
-});
+    // eslint-disable-next-line
+    base: `${env.VITE_PATH}/` ? env.VITE_PATH : '/'
+  }
+})
